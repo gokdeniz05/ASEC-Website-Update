ec1e33d341d99a17fd0a106bb65442f05543a15f
Emre Işık <emremre0770@gmail.com>
2025-11-26 15:44:05 +0300

    CSS dosya yollari duzeltildi

--- src/includes/head-meta.php
+++ src/includes/head-meta.php
@@ -3,23 +3,25 @@
  * ASEC Kulübü - Head Meta Etiketleri
  * Bu dosya, tüm sayfalarda kullanılacak ortak head meta etiketlerini içerir.
  */
-require_once __DIR__ . '/lang.php';
+
+// Lang dosyasını güvenli çağır
+if (file_exists(__DIR__ . '/lang.php')) {
+    require_once __DIR__ . '/lang.php';
+}
 ?>
-<!-- Meta Etiketleri -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Favicon -->
-<link rel="icon" href="images/gallery/try.png" type="image/png">
-<link rel="shortcut icon" href="images/gallery/try.png" type="image/png">
-<link rel="apple-touch-icon" href="images/gallery/try.png">
-<meta name="msapplication-TileImage" content="images/gallery/try.png">
+<link rel="icon" href="/images/gallery/try.png" type="image/png">
+<link rel="shortcut icon" href="/images/gallery/try.png" type="image/png">
+<link rel="apple-touch-icon" href="/images/gallery/try.png">
+<meta name="msapplication-TileImage" content="/images/gallery/try.png">
 <meta name="msapplication-TileColor" content="#1B1F3B">
 <meta name="theme-color" content="#1B1F3B">
 
-<!-- Ortak CSS Dosyaları -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/header.css">
-<link rel="stylesheet" href="css/footer.css">
+
+<link rel="stylesheet" href="/css/reset.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/css/header.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="/css/footer.css?v=<?php echo time(); ?>">
